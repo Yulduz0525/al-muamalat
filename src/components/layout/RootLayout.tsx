3b8f72af2feb58dd6b472dd1173d29cb8512553f
@yunusovayulduz85 <yunusovayulduz85@gmail.com>
2024-11-07 22:40:59 +0500

    project started

---
+++ src/components/layout/RootLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../index.css'
+const RootLayout = () => {
+  return (
+    <div className='nav'>
+        <div className='text-blue-500'>RootLayout</div>
+    </div>
+  )
+}
+
+export default RootLayout
